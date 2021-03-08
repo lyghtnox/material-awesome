5962880b99a1776a5cd3bca3a937dcba2f5dc04c
Nathaniel Campan <nat.campan@gmail.com>
2021-03-08 21:04:43 +0100

    Add layout parameter to each tag

--- configuration/tags/init.lua
+++ configuration/tags/init.lua
@@ -8,48 +8,56 @@ local tags = {
     icon = icons.code,
     type = 'code',
     defaultApp = apps.default.terminal,
+    layout = awful.layout.suit.tile,
     screen = 1
   },
   {
     icon = icons.firefox,
     type = 'browser',
     defaultApp = apps.default.browser,
+    layout = awful.layout.suit.tile,
     screen = 1
   },
   {
     icon = icons.social,
     type = 'social',
     defaultApp = apps.default.social,
+    layout = awful.layout.suit.max,
     screen = 1
   },
   {
     icon = icons.game,
     type = 'game',
     defaultApp = apps.default.game,
+    layout = awful.layout.suit.floating,
     screen = 1
   },
   {
     icon = icons.vm,
     type = 'vms',
     defaultApp = apps.default.vm,
+    layout = awful.layout.suit.tile,
     screen = 1
   },
   {
     icon = icons.folder,
     type = 'files',
     defaultApp = apps.default.files,
+    layout = awful.layout.suit.tile,
     screen = 1
   },
   {
     icon = icons.music,
     type = 'music',
     defaultApp = apps.default.music,
+    layout = awful.layout.suit.tile,
     screen = 1
   },
   {
     icon = icons.lab,
     type = 'any',
     defaultApp = apps.default.rofi,
+    layout = awful.layout.suit.tile,
     screen = 1
   }
 }
@@ -68,7 +76,7 @@ awful.screen.connect_for_each_screen(
         {
           icon = tag.icon,
           icon_only = true,
-          layout = awful.layout.suit.tile,
+          layout = tag.layout,
           gap_single_client = false,
           gap = 4,
           screen = s,
